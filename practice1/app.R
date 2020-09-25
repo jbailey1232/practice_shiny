57e2ca0652a0d96e8543b232492ef09964b3f582
jbailey1232 <jason.a.bailey@gmail.com>
2020-09-24 22:52:34 -0400

    24Sep2020

--- practice1/app.R
+++ practice1/app.R
@@ -3,27 +3,20 @@ library(ggplot2)
 library(vroom)
 library(plyr)
 
-
 ui <- fluidPage(
-
     titlePanel("Simple Histogram Plot"),
     fileInput("file", NULL, accept = c(".csv", ".tsv")),
-    numericInput("n", "Bin Width", value = 1, min = .01, step = .01),
+    textInput("plot_title", label = "Enter plot title", width = 300, placeholder = "Study ABCD vs Study BCDE"),
+    textInput("x_title", label = "Enter x-axis title", width = 300, placeholder ="e.g., S:N or ECLU"),
+    numericInput("n", "Bin Width", value = 0.15, min = .01, step = .01, width = 150),
     tableOutput("stats"),
     plotOutput("histo2"),
-    ) 
-    
-    
-#    sidebarLayout(
-#        sidebarPanel(),
-#        mainPanel()
-#    )
-
+    plotOutput("histo3")
+)
 
 server <- function(input, output, session) {
     data <- reactive({
         req(input$file)
-        
         ext <- tools::file_ext(input$file$name)
         switch(ext,
                csv = vroom::vroom(input$file$datapath, delim = ","),
@@ -36,15 +29,21 @@ server <- function(input, output, session) {
         ddply(data(), .(Study), summarize, N = length(Study), Mean = mean(Value),
               Median = median(Value), SD = sd(Value))
     })
-
+    
     output$histo2 <- renderPlot({
-        ggplot(data(), aes(x=Value, fill = Study)) + 
+        ggplot(data(), aes(x=Value, fill = Study)) +
             geom_histogram(binwidth = input$n, alpha = .7, position = "identity", aes(y=..density..)) +
-            labs(title = "Simulated ISCP data", 
-                 x = "S:N")
+            labs(title = input$plot_title, x = input$x_title) +
+            theme(plot.title = element_text(hjust = 0.5, size = 20), axis.title.x = element_text(size = 12))
     }, res = 96)
     
-
+    output$histo3 <- renderPlot({
+        ggplot(data(), aes(x=Value, fill = Study)) +
+            facet_grid(rows = vars(Study)) +
+            geom_histogram(binwidth = input$n, alpha = .7, position = "identity", aes(y=..density..)) +
+            labs(title = input$plot_title, x = input$x_title) +
+            theme(plot.title = element_text(hjust = 0.5, size = 20), axis.title.x = element_text(size = 12))
+    }, res = 96)
     
 }
 
